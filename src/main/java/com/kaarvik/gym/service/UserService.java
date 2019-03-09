6c8cbd0b0f4c850413696e1e4460ddc8060f8aab
Zach Kaarvik <zkaarvik@gmail.com>
2019-03-09 11:51:19 -0800

    Add transactional annotations and login method

--- src/main/java/com/kaarvik/gym/service/UserService.java
+++ src/main/java/com/kaarvik/gym/service/UserService.java
@@ -4,6 +4,8 @@ import com.kaarvik.gym.model.User;
 import com.kaarvik.gym.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class UserService {
@@ -11,7 +13,14 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Transactional
     public User register(final User user) {
         return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User login() {
+
+        return null;
     }
 }
