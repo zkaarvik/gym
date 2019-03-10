b58649e516364ab2aa3fc636f5ca3d5dc452b3d4
Zach Kaarvik <zkaarvik@gmail.com>
2019-03-10 16:02:22 -0700

    Replace secret with env variable

--- src/main/java/com/kaarvik/gym/security/SecurityConstants.java
+++ src/main/java/com/kaarvik/gym/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.kaarvik.gym.security;
 
 class SecurityConstants {
 
-    static final String SECRET = "TESTJWTSECRET";    //TODO: Replace with real token, don't store this here!
+    static final String SECRET = System.getenv("GYM_JWT_SECRET");
     static final long EXPIRATION_TIME = 864_000_000; // 10 days
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
